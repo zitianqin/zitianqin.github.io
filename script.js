ddbd791e9f60f1dd6c52089392898a6d55bdf909
Zitian Qin <zitianqin2015@gmail.com>
2025-02-20 01:00:41 +1100

    Remove redirect to canva site

--- script.js
+++
@@ -1,8 +0,0 @@
-// use this to avoid redirects when a user clicks "back" in their browser
-window.location.replace("https://zitianqin.my.canva.site/");
-
-// use this to redirect, a back button call will trigger the redirection again
-window.location.href = "https://zitianqin.my.canva.site/";
-
-// given for completeness, essentially an alias to window.location.href
-window.location = "https://zitianqin.my.canva.site/";
