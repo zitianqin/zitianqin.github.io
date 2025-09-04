db96245bd6920cf7a83e7303e62b183e3906220e
Zitian Qin <zitianqin2015@gmail.com>
2025-09-05 00:16:28 +1000

    Update favicon to chopin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Zitian Qin",
   description: "I like building things.",
+  icons: {
+    icon: "/chopin.png",
+  },
 };
 
 export default function RootLayout({
